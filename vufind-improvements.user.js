ead204caf55dc8c258e97340fd6630142f925310
James Cuénod <jcuenod@users.noreply.github.com>
2018-04-07 22:22:13 -0500

    Added author attribution!

--- vufind-improvements.user.js
+++ vufind-improvements.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       James Cuénod
 // @match        https://vufind.carli.illinois.edu/all/vf/MyResearch/CheckedOut
 // @grant        none
 // ==/UserScript==
